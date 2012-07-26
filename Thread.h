91f4d63bf5e765acb2c06d04af002ebb61768f8a
ytt <ytt@ytt.ytt>
2012-07-26 15:39:54 +0900

    Thread リファクタリング

--- Thread.h
+++ Thread.h
@@ -25,8 +25,8 @@ private:
 		// スレッド終了時にインスタンスを削除するか
 		bool m_isDeleteInstance;
 	public:
-		ThreadDataBase() :
-				m_isDeleteInstance(false)
+		ThreadDataBase(bool isDeleteInstance) :
+				m_isDeleteInstance(isDeleteInstance)
 		{
 		}
 		void setDeleteInstance(bool value)
@@ -37,7 +37,7 @@ private:
 		{
 		}
 		// スレッドを開始します。
-		virtual void startThread() = 0;
+		virtual void startThread()=0;
 	};
 	/*
 	 * スレッドで実行するインスタンスとメソッドを保持するクラス
@@ -45,74 +45,67 @@ private:
 	template<typename T>
 	class ThreadData: public ThreadDataBase
 	{
+
 	public:
-		ThreadData() :
-				instance(NULL), method(NULL)
+        //　メソッドを実行するインスタンスです。
+        T* m_instance;
+        // 実行するメソッドです。
+        void* m_method;
+		ThreadData(T* instance,bool isDeleteInstance) :ThreadDataBase(isDeleteInstance),
+				m_instance(instance)
 		{
+
 		}
 		virtual ~ThreadData()
 		{
-			free(method);
+		    if (this->m_isDeleteInstance){
+		        delete this->m_instance;
+		    }
+			free(m_method);
 		}
-		//　メソッドを実行するインスタンスです。
-		void* instance;
-		// 実行するメソッドです。
-		void* method;
-
 		//override スレッドを開始します。
 		void startThread()
 		{
 			typedef void (T::*method_t)(void);
 
-			method_t method_ = *(method_t*) method;
-			T *instance_ = (T*) instance;
-			(instance_->*method_)();
-			if (m_isDeleteInstance){
-				delete instance_;
-			}
+			method_t method_ = *(method_t*) m_method;
+			(m_instance->*method_)();
 		}
 	};
 	template<typename T, typename TArg,typename TArgActual>
-	class TrehadDataFunc: public ThreadData<T>
+	class ThreadDataFunc: public ThreadData<T>
 	{
 	public:
-		virtual ~TrehadDataFunc()
+	    ThreadDataFunc(T* instance,bool isDeleteInstance) :ThreadData<T>(instance,isDeleteInstance)
+	    {}
+		virtual ~ThreadDataFunc()
 		{
 		}
-		TArgActual arg;
+        TArgActual arg;
 		//override スレッドを開始します。
 		void startThread()
 		{
 			typedef void (T::*method_t)(TArg);
-
-			method_t method_ = *(method_t*)this->method;
-			T *instance_ = (T*)this->instance;
-			(instance_->*method_)(arg);
-			if (this->m_isDeleteInstance){
-				delete instance_;
-			}
+			method_t method_ = *(method_t*)this->m_method;
+			(this->m_instance->*method_)(arg);
 		}
 	};
 	template<typename T, typename TArg,typename TArgActual, typename TArg2,typename TArgActual2>
-	class TrehadDataFunc2: public ThreadData<T>
+	class ThreadDataFunc2: public ThreadDataFunc<T,TArg,TArgActual>
 	{
 	public:
-		virtual ~TrehadDataFunc2()
+	    ThreadDataFunc2(T* instance,bool isDeleteInstance) :ThreadDataFunc<T,TArg,TArgActual>(instance,isDeleteInstance)
+	           {}
+		virtual ~ThreadDataFunc2()
 		{
 		}
-		TArgActual arg;
-		TArgActual2 arg2;
+        TArgActual2 arg2;
 		//override スレッドを開始します。
 		void startThread()
 		{
 			typedef void (T::*method_t)(TArg,TArg2);
-
-			method_t method_ = *(method_t*)this->method;
-			T *instance_ = (T*)this->instance;
-			(instance_->*method_)(arg,arg2);
-			if (this->m_isDeleteInstance){
-				delete instance_;
-			}
+			method_t method_ = *(method_t*)this->m_method;
+			(this->m_instance->*method_)(this->arg,arg2);
 		}
 	};
 	// Thread実行のための要素です。
@@ -141,13 +134,11 @@ public:
 	template<typename T>
 	Thread(T *obje, void (T::*method)(void), bool isDeleteInstance = false)
 	{
-		ThreadData<T> *data = new ThreadData<T>();
-		data->instance = (void*) obje;
-		data->setDeleteInstance(isDeleteInstance);
+		ThreadData<T> *data = new ThreadData<T>(obje,isDeleteInstance);
 		typedef void (T::*method_t)(void);
 		int size = sizeof(method_t);
-		data->method = malloc(size);
-		*(method_t*) data->method = method;
+		data->m_method = malloc(size);
+		*(method_t*) data->m_method = method;
 
 		threadData = data;
 	}
@@ -156,14 +147,12 @@ public:
 	Thread(T *obje, void (T::*method)(TArg), TArgActual arg, bool isDeleteInstance =
 	        false)
 	{
-		TrehadDataFunc<T, TArg, TArgActual> *data = new TrehadDataFunc<T,TArg, TArgActual>();
-		data->instance = (void*) obje;
-		data->setDeleteInstance(isDeleteInstance);
+	    ThreadDataFunc<T, TArg, TArgActual> *data = new ThreadDataFunc<T,TArg, TArgActual>(obje,isDeleteInstance);
 		data->arg = arg;
 		typedef void (T::*method_t)(TArg);
 		int size = sizeof(method_t);
-		data->method = malloc(size);
-		*(method_t*) data->method = method;
+		data->m_method = malloc(size);
+		*(method_t*) data->m_method = method;
 		threadData = data;
 	}
 	// インスタンスとメソッドを指定する
@@ -171,15 +160,14 @@ public:
 	Thread(T *obje, void (T::*method)(TArg,TArg2), TArgActual arg,TArgActual2 arg2, bool isDeleteInstance =
 	        false)
 	{
-		TrehadDataFunc2<T, TArg,TArgActual,TArg2, TArgActual2> *data = new TrehadDataFunc2<T, TArg,TArgActual,TArg2, TArgActual2>();
-		data->instance = (void*) obje;
-		data->setDeleteInstance(isDeleteInstance);
+	    ThreadDataFunc2<T, TArg,TArgActual,TArg2, TArgActual2> *data =
+		        new ThreadDataFunc2<T, TArg,TArgActual,TArg2, TArgActual2>(obje,isDeleteInstance);
 		data->arg = arg;
 		data->arg2 = arg2;
 		typedef void (T::*method_t)(TArg,TArg2);
 		int size = sizeof(method_t);
-		data->method = malloc(size);
-		*(method_t*) data->method = method;
+		data->m_method = malloc(size);
+		*(method_t*) data->m_method = method;
 		threadData = data;
 	}
 	virtual ~Thread()
